75d8431969c29743b6fc73a5d0cd644c9866c3b4
Flairie <37420327+Flairie@users.noreply.github.com>
2019-04-13 16:09:27 +0300

    Create misha.php

---
+++ misha.php
@@ -0,0 +1,56 @@
+<?php
+if (!isset($_REQUEST)) {
+return;
+}
+
+//Строка для подтверждения адреса сервера из настроек Callback API
+$confirmation_token = '365dc053';
+
+//Ключ доступа сообщества
+$token = '672d453ece22cb4a900144f2bf11f0083af5defd1bcd5db3883a48e5d2867906e26ff64409295cc2e3496';
+
+//Получаем и декодируем уведомление
+$data = json_decode(file_get_contents('php://input'));
+
+//Проверяем, что находится в поле "type"
+switch ($data->type) {
+//Если это уведомление для подтверждения адреса...
+case 'confirmation':
+//...отправляем строку для подтверждения
+echo $confirmation_token;
+break;
+
+//Если это уведомление о новом сообщении...
+case 'message_new':
+//...получаем id его автора
+$user_id = $data->object->user_id;
+//затем с помощью users.get получаем данные об авторе
+$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.0"));
+
+//и извлекаем из ответа его имя
+$user_name = $user_info->response[0]->first_name;
+
+//С помощью messages.send отправляем ответное сообщение
+$request_params = array(
+'message' => "Hello, {$user_name}!",
+'user_id' => $user_id,
+'access_token' => $token,
+'v' => '5.0'
+);
+
+$get_params = http_build_query($request_params);
+
+file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
+
+//Возвращаем "ok" серверу Callback API
+
+echo('ok');
+
+break;
+
+}
+?> 
+
+
+
+?>
